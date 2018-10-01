c04d2948d011adbc0a2e7f75cf5438b88e096d06
RemZiQ <remziq1992@gmail.com>
2018-10-01 04:34:18 +0300

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 > 0){
+  	return false;
+  }
+  let i,
+  	j,
+  	length = str.length,
+  	configLength = bracketsConfig.length,
+  	count = 0;
+
+  	for (i = 0; i < length; i++){
+  		for (j = 0; j < configLength; j++){
+  			if(str[i] == bracketsConfig[j][0]) count++;
+  			else if(str[i] == bracketsConfig[j][1]) count --;
+  		}
+  	}
+  	return count == 0 ? true : false;
 }
